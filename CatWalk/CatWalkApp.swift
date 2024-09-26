cc4a4fb9e4b5400c8d485ab8c0c23d5f0a9dc866
carrie <771685809@qq.com>
2024-09-26 16:10:44 +0800

    Initial Commit

---
+++ CatWalk/CatWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatWalkApp.swift
+//  CatWalk
+//
+//  Created by  玉城 on 2024/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CatWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatWalk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatWalk
+//
+//  Created by  玉城 on 2024/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
